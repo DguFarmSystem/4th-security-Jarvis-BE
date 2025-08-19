c953f71dfe3a2afd96411e841a6eb203aa753270
CometWoo <a01037878249@gamil.com>
2025-08-19 16:23:32 +0900

    [test] cert#6

--- auth/github.go
+++ auth/github.go
@@ -101,6 +101,5 @@ func (h *Handler) HandleGitHubCallback(c *gin.Context) {
 
 	c.SetCookie("auth_token", tokenString, 3600, "/", "", true, false)
 
-	//c.Redirect(http.StatusFound, "https://jarvis-indol-omega.vercel.app")
-	c.Redirect(http.StatusFound, "https://openswdev.duckdns.org:3000/")
+	c.Redirect(http.StatusFound, "https://jarvis-indol-omega.vercel.app")
 }
