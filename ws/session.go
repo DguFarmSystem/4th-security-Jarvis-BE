912003a51510cb3ca275c8d45c0cd73f7f466680
Choi Yunho <cyunho62100@dgu.ac.kr>
2025-08-21 03:04:32 +0900

    [fix] add force option

--- ws/session.go
+++ ws/session.go
@@ -43,6 +43,7 @@ func HandleWebSocket(c *gin.Context) {
 		"--user", githubUser,
 		"--out", tmpfile.Name(),
 		"--format=openssh",
+		"--force",
 		"--ttl=1m",
 	)
 	if output, err := authSignCmd.CombinedOutput(); err != nil {
