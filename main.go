85776c112c445a4ca58b8a0cf192b4d0a5e74b30
Suhyeon Park <ssantta999@gmail.com>
2025-09-27 14:56:59 +0900

    set cors

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"teleport-backend/config"
 	"teleport-backend/ws"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -31,6 +32,15 @@ func main() {
 
 	// Gin 라우터 설정
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	// 라우트 등록
 	router.POST("/login", authHandler.HandleLogin)
